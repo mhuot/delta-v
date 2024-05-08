919b8b59fa4fbcdb09329803fa85bbbabe795feb
Markus von Rüden <m.v.rueden@open-email.de>
2024-05-08 19:47:46 +0200

    chg: remove usage statistics (aka datachoices)

--- smoke-test/src/test/java/org/opennms/smoketest/AdminPasswordGateIT.java
+++ smoke-test/src/test/java/org/opennms/smoketest/AdminPasswordGateIT.java
@@ -74,8 +74,7 @@ public class AdminPasswordGateIT extends OpenNMSSeleniumIT {
         LOG.debug("Test admin login with new password");
         logout();
         // skip cookie deletion, we need to retain for authorization for resetPassword Rest API call
-        // do not navigate to login page, we should already be there
-        login(PASSWORD_GATE_USERNAME, ALTERNATE_ADMIN_PASSWORD, true, true, false, true);
+        login(PASSWORD_GATE_USERNAME, ALTERNATE_ADMIN_PASSWORD, true, true, true);
         assertTrue(driver.getCurrentUrl().contains("index.jsp"));
         verifyOnMainPage();
 
@@ -97,7 +96,7 @@ public class AdminPasswordGateIT extends OpenNMSSeleniumIT {
         logout();
         nodePage();
         waitFor("login.jsp");
-        login(PASSWORD_GATE_USERNAME, PASSWORD_GATE_PASSWORD, true, false, false, false);
+        login(PASSWORD_GATE_USERNAME, PASSWORD_GATE_PASSWORD, true, false, false);
         waitFor("element/nodeList.htm");
 
         // logout and try to go to a non-login page
@@ -135,7 +134,7 @@ public class AdminPasswordGateIT extends OpenNMSSeleniumIT {
 
     private void loginAndChangePassword(String expectedRedirectUrl, boolean navigateToLoginPage) {
         // login with "admin/admin", do not skip past the password gate, skip cookie deletion
-        login(PASSWORD_GATE_USERNAME, PASSWORD_GATE_PASSWORD, false, false, navigateToLoginPage, true);
+        login(PASSWORD_GATE_USERNAME, PASSWORD_GATE_PASSWORD, false, navigateToLoginPage, true);
 
         if (!driver.getCurrentUrl().contains("passwordGate.jsp")) {
             fail("Failed to get password gate page after 'admin/admin' login attempt.");
@@ -153,7 +152,7 @@ public class AdminPasswordGateIT extends OpenNMSSeleniumIT {
 
     private void loginAndSkip() {
         // login with "admin/admin", should succeed but display passwordGate page, which is skipped
-        login(PASSWORD_GATE_USERNAME, PASSWORD_GATE_PASSWORD, true, true, true, false);
+        login(PASSWORD_GATE_USERNAME, PASSWORD_GATE_PASSWORD, true, true, false);
 
         waitFor("index.jsp");
     }
