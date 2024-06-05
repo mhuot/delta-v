d91925b382c7fa5ef6e2f5deff2da26ff2a7854e
Markus von Rüden <m.v.rueden@open-email.de>
2024-06-05 21:03:24 +0200

    chg: remove tl1d

--- core/upgrade/src/main/java/org/opennms/upgrade/implementations/EOLServiceConfigMigratorOffline.java
+++ core/upgrade/src/main/java/org/opennms/upgrade/implementations/EOLServiceConfigMigratorOffline.java
@@ -134,7 +134,10 @@ public class EOLServiceConfigMigratorOffline extends AbstractOnmsUpgrade {
                 "OpenNMS:Name=Xmlrpcd",
                 "OpenNMS:Name=XmlrpcProvisioner",
                 "OpenNMS:Name=AccessPointMonitor",
-                "OpenNMS:Name=PollerBackEnd"
+                "OpenNMS:Name=PollerBackEnd",
+                "OpenNMS:Name=Reportd",
+                "OpenNMS:Name=Statsd",
+                "OpenNMS:Name=Tl1d"
         };
 
         try {
--- smoke-test/src/test/java/org/opennms/smoketest/UsageStatisticsIT.java
+++ smoke-test/src/test/java/org/opennms/smoketest/UsageStatisticsIT.java
@@ -85,9 +85,9 @@ public class UsageStatisticsIT {
 
         final Map<String, Boolean> services = (Map<String, Boolean>) usageReport.get("services");
 
-        assertEquals(28, services.size());
-        assertEquals(26, services.entrySet().stream().filter(Map.Entry::getValue).count());
-        assertEquals(2, services.entrySet().stream().filter(e -> !e.getValue()).count());
+        assertEquals(29, services.size());
+        assertEquals(26, services.entrySet().stream().filter(e -> e.getValue()).count());
+        assertEquals(3, services.entrySet().stream().filter(e -> !e.getValue()).count());
 
         assertThat((String) usageReport.get("systemId"), matchesPattern("^\\S+-\\S+-\\S+-\\S+-\\S+$"));
 
