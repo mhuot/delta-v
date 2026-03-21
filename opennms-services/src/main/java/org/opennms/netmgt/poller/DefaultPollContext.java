66b7eb8a046793fdd2475aeabd2cb7dbd0affe70
David Hustace <david@beaconstrategists.com>
2026-03-21 07:29:07 -0700

    fix: resolve alarm-data placeholders in DefaultPollContext events
    Build fully-resolved reduction keys (e.g., uei:dpname:nodeid:iface:svc)
    instead of using eventconf placeholders (%uei%:%dpname% etc.) that
    require Eventd enrichment. Standalone daemons don't have EventConfDao.

--- opennms-services/src/main/java/org/opennms/netmgt/poller/DefaultPollContext.java
+++ opennms-services/src/main/java/org/opennms/netmgt/poller/DefaultPollContext.java
@@ -39,6 +39,7 @@ import org.opennms.netmgt.icmp.proxy.PingSequence;
 import org.opennms.netmgt.icmp.proxy.PingSummary;
 import org.opennms.netmgt.model.events.EventBuilder;
 import org.opennms.netmgt.xml.event.AlarmData;
+import org.opennms.netmgt.xml.event.Event;
 import org.opennms.netmgt.poller.pollables.DbPollEvent;
 import org.opennms.netmgt.poller.pollables.PollContext;
 import org.opennms.netmgt.poller.pollables.PollEvent;
@@ -207,44 +208,50 @@ public class DefaultPollContext implements PollContext, InitializingBean {
     /**
      * Adds alarm-data to Pollerd events so Alarmd can create alarms.
      * Matches the eventconf definitions in opennms.pollerd.events.xml.
-     * In the monolith, Eventd enriches events via eventconf. In standalone
-     * daemon containers without EventConfDao, we set alarm-data directly.
+     * In the monolith, Eventd enriches events via eventconf and expands
+     * %uei%, %dpname%, etc. In standalone daemon containers, we must
+     * set alarm-data with fully-resolved reduction keys.
      */
     private void addAlarmData(EventBuilder bldr, String uei) {
+        Event event = bldr.getEvent();
+        String dpname = event.getDistPoller() != null ? event.getDistPoller() : "localhost";
+        String nodeid = event.getNodeid() != null ? String.valueOf(event.getNodeid()) : "";
+        String iface = event.getInterface() != null ? event.getInterface() : "";
+        String svc = event.getService() != null ? event.getService() : "";
+
         AlarmData alarmData = new AlarmData();
         switch (uei) {
             case EventConstants.NODE_LOST_SERVICE_EVENT_UEI:
-                alarmData.setReductionKey("%uei%:%dpname%:%nodeid%:%interface%:%service%");
+                alarmData.setReductionKey(uei + ":" + dpname + ":" + nodeid + ":" + iface + ":" + svc);
                 alarmData.setAlarmType(1);
                 alarmData.setAutoClean(false);
                 bldr.setAlarmData(alarmData);
                 bldr.setSeverity("Minor");
                 break;
             case EventConstants.NODE_REGAINED_SERVICE_EVENT_UEI:
-                alarmData.setReductionKey("%uei%:%dpname%:%nodeid%:%interface%:%service%");
+                alarmData.setReductionKey(uei + ":" + dpname + ":" + nodeid + ":" + iface + ":" + svc);
                 alarmData.setAlarmType(2);
-                alarmData.setClearKey("uei.opennms.org/nodes/nodeLostService:%dpname%:%nodeid%:%interface%:%service%");
+                alarmData.setClearKey(EventConstants.NODE_LOST_SERVICE_EVENT_UEI + ":" + dpname + ":" + nodeid + ":" + iface + ":" + svc);
                 alarmData.setAutoClean(false);
                 bldr.setAlarmData(alarmData);
                 bldr.setSeverity("Normal");
                 break;
             case EventConstants.NODE_DOWN_EVENT_UEI:
-                alarmData.setReductionKey("%uei%:%dpname%:%nodeid%");
+                alarmData.setReductionKey(uei + ":" + dpname + ":" + nodeid);
                 alarmData.setAlarmType(1);
                 alarmData.setAutoClean(false);
                 bldr.setAlarmData(alarmData);
                 bldr.setSeverity("Major");
                 break;
             case EventConstants.NODE_UP_EVENT_UEI:
-                alarmData.setReductionKey("%uei%:%dpname%:%nodeid%");
+                alarmData.setReductionKey(uei + ":" + dpname + ":" + nodeid);
                 alarmData.setAlarmType(2);
-                alarmData.setClearKey("uei.opennms.org/nodes/nodeDown:%dpname%:%nodeid%");
+                alarmData.setClearKey(EventConstants.NODE_DOWN_EVENT_UEI + ":" + dpname + ":" + nodeid);
                 alarmData.setAutoClean(false);
                 bldr.setAlarmData(alarmData);
                 bldr.setSeverity("Normal");
                 break;
             default:
-                // No alarm-data for other events
                 break;
         }
     }
