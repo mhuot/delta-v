cc47303c6bf99033ca6346556303ffd9a3699744
David Hustace <david@beaconstrategists.com>
2026-03-23 14:48:21 -0700

    feat: update Docker integration for Spring Boot Enlinkd

--- opennms-container/delta-v/build.sh
+++ opennms-container/delta-v/build.sh
@@ -163,6 +163,7 @@ do_stage_daemon_jars() {
         "core/daemon-boot-bsmd/target/org.opennms.core.daemon-boot-bsmd-$VERSION-boot.jar:daemon-boot-bsmd.jar"
         "core/daemon-boot-perspectivepollerd/target/org.opennms.core.daemon-boot-perspectivepollerd-$VERSION-boot.jar:daemon-boot-perspectivepollerd.jar"
         "core/daemon-boot-telemetryd/target/org.opennms.core.daemon-boot-telemetryd-$VERSION-boot.jar:daemon-boot-telemetryd.jar"
+        "core/daemon-boot-enlinkd/target/org.opennms.core.daemon-boot-enlinkd-$VERSION-boot.jar:daemon-boot-enlinkd.jar"
         # Special JARs (EventTranslator split-package fix, Alarmd, Passive status)
         "opennms-config/target/opennms-config-$VERSION.jar:opennms-config.jar"
         "opennms-util/target/opennms-util-$VERSION.jar:opennms-util.jar"
